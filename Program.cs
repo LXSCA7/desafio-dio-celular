d8c479f5e7d4ea8b170a796a4ea73570bdf49928
LXSCA <lucas.edr27@gmail.com>
2024-05-15 17:58:22 -0300

    resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("REALIZAÇÃO DE TESTES PARA A ABSTRAÇÃO DE UM SMARTPHONE");
+Console.WriteLine("\nTESTE 01 - SMARTPHONE NOKIA\n");
+Nokia nokia = new Nokia("+55 21 96122-3341", "Lumia 950", "353612650656221", 128);
+nokia.Informacoes();
+nokia.Ligar();
+nokia.InstalarAplicativo("Spotify");
+
+Console.WriteLine("\nTESTE 02: SMARTPHONE IPHONE...\n");
+Iphone iphone = new Iphone("+55 11 95154-4163", "Iphone 15 Pro", "353864165920218", 256);
+iphone.Informacoes();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
